856614de3d43bd52ab0e63a1ce21e4c4f72dd656
Wan Isa <wiwm88@gmail.com>
2026-05-17 11:54:54 +0800

    feat: add Loading component for improved user experience during data fetching

---
+++ app/loading.tsx
@@ -0,0 +1,21 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 py-80">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-10 w-10 animate-spin"
+      >
+        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+      </svg>
+      Loading...
+    </div>
+  );
+}
