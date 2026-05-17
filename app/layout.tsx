72f36789d08517435af48006b10c5b157c2ad8f0
Wan Isa <wiwm88@gmail.com>
2026-05-17 11:54:44 +0800

    feat: integrate AuthProvider and session management in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
+import AuthProvider from "@/components/auth-provider";
+import { auth, getSession } from "@/lib/auth";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -10,24 +12,25 @@ export const metadata: Metadata = {
   title: "Inventory Request Module",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getSession();
+
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", inter.className, "dark")}
-    >
+    <html lang="en" className={cn("dark h-full antialiased", inter.className)}>
       <body>
-        <main className="mx-auto max-w-200 px-4">
-          {children}
-          <nav className="text-muted-foreground mt-4 py-4 text-center text-xs">
-            Copyright @ {new Date().getFullYear()} Wan Isa
-          </nav>
-        </main>
-        <Toaster />
+        <AuthProvider session={session as typeof auth.$Infer.Session}>
+          <main className="mx-auto max-w-200 px-4">
+            {children}
+            <nav className="text-muted-foreground mt-4 py-4 text-center text-xs">
+              Copyright @ {new Date().getFullYear()} Wan Isa
+            </nav>
+          </main>
+          <Toaster />
+        </AuthProvider>
       </body>
     </html>
   );
